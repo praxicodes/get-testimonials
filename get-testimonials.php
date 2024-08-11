e1c94b8a441cef787d6c2780dc17ec4b6842fadc
Prakritee Sharma <prakritee08@gmail.com>
2024-08-11 14:59:55 +0545

    feat: wp scaffold plugin for initial setup

---
+++ get-testimonials.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     Get Testimonials
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     A simple plugin to store testimonials given by website visitors.
+ * Author:          Prakritee Sharma
+ * Author URI:      https://github.com/praxicodes
+ * Text Domain:     get-testimonials
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         Get_Testimonials
+ */
+
+//Require vendor here
+require 'vendor/autoload.php';
